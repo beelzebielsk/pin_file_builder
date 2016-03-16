3e2fcfcaa45d9bb554b28fda0ab40321553e7784
adam <ibrahimadam193@gmail.com>
2016-03-15 23:44:17 -0400

    Fixed a bug where descriptive pin names had to be uppercase. They can  now be entered in any case, including mixed case.  Changes to be committed:

--- replacer.js
+++ replacer.js
@@ -1,33 +1,26 @@
 "use strict"
 var basicLine = require("./validation.js").afterParseLine;
 var blankLine = require("./validation.js").blankLine;
+var pinTable = require("./pin_table.json");
 var fs = require("fs");
-var programDirectory = ( process.env.PROG_DIR ? process.env.PROG_DIR : "./" );
 
 // Set up input and output to and from program.
+// TODO: Give option to take input from and write input to
+// specified files.
 var input = process.stdin;
 var output = process.stdout;
 output.write("TO, LOCATION\n");
 
-// Get the pin table and file to run replacer on.
-var pinTableStream = fs.createReadStream(programDirectory + "pin_table.json");
+// Get the and file to run replacer on.
 var fileContent = "";
-var pinTable = "";
 input.on('data', (chunk) => { fileContent += chunk; } );
-input.on('end', () => { 
-	pinTableStream.on('data', (chunk) => { pinTable += chunk; } );
-	pinTableStream.on('end', () => { 
-		pinTable = JSON.parse(pinTable);
-		replace();
-	} );
-} );
+input.on('end', () => replace() );
 
 
 function replace() {
 	var lines = fileContent.trim().split('\n');
 	//console.log( lines ); //DEBUG
 	//console.log( basicLine ); //DEBUG
-	//console.log(JSON.stringify(pinTable,null,3) ); //DEBUG
 	for (var line of lines) {
 		if (line.match(blankLine)) {
 			output.write(line);
@@ -41,7 +34,11 @@ function replace() {
 			if (pinName === "LOCATION")
 				continue;
 			//console.log(pinName); //DEBUG
-			output.write(line.replace(pinName, pinTable[pinName]) + '\n');
+			//The below line won't work because 'pinName' doesn't
+			//match the original thing if the original thing wasn't
+			//uppercase.
+			//output.write(line.replace(pinName, pinTable[pinName]) + '\n');
+			output.write(line.replace(match[2], pinTable[pinName]) + '\n');
 		}
 	}
 }
